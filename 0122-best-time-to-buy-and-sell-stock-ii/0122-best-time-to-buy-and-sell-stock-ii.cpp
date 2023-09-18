833a2facdf38ca2aee7b5fcd6b4539b93dee69c5
POKUDING <92212524+POKUDING@users.noreply.github.com>
2023-09-18 19:04:29 +0900

    Time: 0 ms (100%), Space: 13.3 MB (37.88%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int ret = 0;
-        int hold = -1;
         ios::sync_with_stdio(0);
         cin.tie(0);
 
-        for (int i = 0, size = prices.size(); i < size; ++i)
+        for (int i = 1, size = prices.size(); i < size; ++i)
         {
-            if (hold >= 0 && prices[i] > hold)
-            {
-                ret += prices[i] - hold;
-                hold = -1;
-            }
-            if (hold < 0 && i < size - 1 && prices[i] < prices[i + 1])
-                hold = prices[i];
+            if (prices[i] > prices[i - 1])
+                ret += prices[i] - prices[i - 1];
         }
         return ret;
     }
