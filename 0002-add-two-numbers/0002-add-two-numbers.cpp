a27891edb4525ef5b7b715ae2f0b81e811b9c62f
POKUDING <92212524+POKUDING@users.noreply.github.com>
2023-09-20 18:18:32 +0900

    Time: 19 ms (85.68%), Space: 71.9 MB (6.81%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -14,26 +14,21 @@ public:
         ios::sync_with_stdio(0);
         cin.tie(0);
         int tmp = 0;
-        int val1, val2;
         ListNode *rtn = new ListNode();
         ListNode *currptr = rtn;
 
         while (l1 || l2 || tmp)
         {
-            val1 = 0;
-            val2 = 0;
             if (l1)
             {
-                val1 = l1->val;
+                tmp += l1->val;
                 l1 = l1->next;
             }
             if (l2)
             {
-                val2 = l2->val;
+                tmp += l2->val;
                 l2 = l2->next;
             }
-            tmp += val1 + val2;
-            cout << val1 << " " << val2 << "\n";
             currptr->next = new ListNode(tmp % 10);
             currptr = currptr->next;
             tmp /= 10;
