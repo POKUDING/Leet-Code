1420a167d78d22aea56238d150cf3e29915d341e
POKUDING <92212524+POKUDING@users.noreply.github.com>
2023-09-23 16:28:14 +0900

    Time: 131 ms (44.05%), Space: 61.9 MB (30.77%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    static bool compareByLength(std::string& a, std::string& b) {
+        return a.length() < b.length();
+    }
+    int longestStrChain(vector<string>& words) {
+        int rtn = 0;
+        unordered_map<string, int> map;
+        int longest;
+
+        sort(words.begin(), words.end(), compareByLength);
+        for(string s : words)
+        {
+            longest = 0;
+            for (int i = 0, size = s.length(); i < size; ++i)
+            {
+                string tmp(s);
+                tmp.erase(i, 1);
+                longest = max(longest, map[tmp] + 1);
+            }
+            map[s] = longest;
+            rtn = max(rtn, longest);
+        }
+        return rtn;
+    }
+};
