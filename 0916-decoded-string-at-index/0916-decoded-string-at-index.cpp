d9c2a4e2cf268daa0ad8fc2f0e88f5bea51648bd
POKUDING <92212524+POKUDING@users.noreply.github.com>
2023-09-28 18:31:25 +0900

    Time: 0 ms (100%), Space: 6.4 MB (9.07%) - LeetHub

---
+++ 0916-decoded-string-at-index/0916-decoded-string-at-index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        long long len = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (isdigit(s[i]))
+                len *= s[i] - '0';
+            else
+                ++len;
+        }
+
+        for (int i = s.size() - 1; i >= 0; --i) {
+            if(isdigit(s[i])) {
+                len /= (s[i] - '0');
+                k = k % len;
+            } else {
+                if (k == 0 || len == k)
+                    return s.substr(i, 1);
+                --len;
+            }
+        }
+        return "";
+    }
+};
