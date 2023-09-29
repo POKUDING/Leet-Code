6e9aa83eeccaf7e7542d02b73b50f39781c51aa1
POKUDING <92212524+POKUDING@users.noreply.github.com>
2023-09-29 15:42:56 +0900

    Time: 85 ms (99.26%), Space: 96.7 MB (22.05%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
+        ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
         int size = nums.size();
+        int flag = 0;
         int i = 0;
-        while(i < size - 1 && nums[i] <= nums[i + 1])
+        while(i < size - 1)
+        {
+            if (nums[i] != nums[i + 1])
+            {
+                flag = nums[i] - nums[i + 1];
+                break;
+            }
             ++i;
-        if (i == size - 1)
-            return true;
-        i = 0;
-        while(i < size - 1 && nums[i] >= nums[i + 1])
+        }
+        while(i < size - 1)
+        {
+            if (flag > 0 && nums[i] < nums[i + 1])
+                return false;
+            else if (flag < 0 && nums[i] > nums[i + 1])
+                return false;
             ++i;
-        if (i == size -1)
-            return true;
-        return false;
+        }
+        return true;
     }
 };
