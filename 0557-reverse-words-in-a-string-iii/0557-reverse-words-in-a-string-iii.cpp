88a1f354907ff2e7c68e4cff4ac18956561a5928
POKUDING <92212524+POKUDING@users.noreply.github.com>
2023-10-01 15:26:18 +0900

    Time: 14 ms (65.92%), Space: 9.9 MB (59.58%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string::iterator it = s.begin();
+        string::iterator begin_it = s.begin();
+        int size = s.size();
+        for(int i = 0; i <= size; ++i)
+        {
+            if (i == size || isspace(s[i]))
+            {
+                reverse(it,begin_it + i);
+                it = begin_it + i + 1;
+            }
+        }
+        return s;
+    }
+};
