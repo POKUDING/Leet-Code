7747941533b396df5224bf5a24318d98bff65061
POKUDING <92212524+POKUDING@users.noreply.github.com>
2023-10-01 15:30:02 +0900

    Time: 8 ms (96.47%), Space: 9.9 MB (59.58%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         string::iterator it = s.begin();
         string::iterator begin_it = s.begin();
         int size = s.size();
         for(int i = 0; i <= size; ++i)
         {
-            if (i == size || isspace(s[i]))
+            if (i == size || s[i] == ' ')
             {
                 reverse(it,begin_it + i);
                 it = begin_it + i + 1;
