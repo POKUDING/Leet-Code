429a6069a4bbf05d8091d86ee2c8f7a4fb73562b
POKUDING <92212524+POKUDING@users.noreply.github.com>
2023-10-02 22:56:56 +0900

    Time: 36 ms (38.86%), Space: 13.9 MB (7.8%) - LeetHub

---
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+       int size = colors.size();
+       int cnt = 0;
+       int a = 0;
+       int b = 0;
+       for(int i = 0; i < size; ++i)
+       {
+            if (colors[i] != 'A')
+                cnt = 0;
+            else if (++cnt >= 3)
+                ++a;
+       }
+       cnt = 0;
+       for(int i = 0; i < size; ++i)
+       {
+            if (colors[i] != 'B')
+                cnt = 0;
+            else if (++cnt >= 3)
+                ++b;
+       }
+        if (a <= b)
+            return false;
+        return true;
+    }
+};
