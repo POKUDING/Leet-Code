5fa55ed6ba0c52b2ca7c43b97184a7ed0eb92224
POKUDING <92212524+POKUDING@users.noreply.github.com>
2023-10-17 12:19:35 +0900

    Time: 67 ms (27.35%), Space: 32.8 MB (69.45%) - LeetHub

---
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        vector<int> par(n, -1);
+        queue<int> que;
+
+        for (int i = 0; i < n; ++i)
+        {
+            int left = leftChild[i];
+            int right = rightChild[i];
+            if (left != -1)
+            {
+                if (par[left] != -1)
+                    return false;
+                par[left] = i;
+            }
+            if (right != -1)
+            {
+                if (par[right] != -1)
+                    return false;
+                par[right] = i;
+            }
+        }
+        vector<int>::iterator it = find(par.begin(), par.end(), -1);
+        cout<< "here1\n";
+        if (it == par.end())
+            return false;
+
+        que.push(it - par.begin());
+        while(!que.empty())
+        {
+            cout << que.front() << "\n";
+            par[que.front()] = -2;
+            if (leftChild[que.front()] != -1)
+                que.push(leftChild[que.front()]);
+            if (rightChild[que.front()] != -1)
+                que.push(rightChild[que.front()]);
+            que.pop();
+        }
+        cout<< "here2\n";
+        for(int i = 0; i < n; ++i)
+        {
+            cout << par[i] << " ";
+            if(par[i] != -2)
+                return false;
+        }
+        return true;
+    }
+};
