8070eb4f6f671bf5ecd9a471dcf4dac7008b4b1b
JunHyung Park <yoseb95@gmail.com>
2024-01-21 12:30:37 +0900

    Time: 64 ms (18.98%), Space: 51.6 MB (5.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.ts
+++ 0198-house-robber/0198-house-robber.ts
@@ -3,8 +3,7 @@ function rob(nums: number[]): number {
 
     if(nums.length > 1)
         moneys.push(Math.max(nums[0],nums[1]));
-    for (let i = 2; i < nums.length; ++i) {
+    for (let i = 2; i < nums.length; ++i)
         moneys.push(Math.max(nums[i] + moneys[i - 2], moneys[i - 1]));
-    }
     return moneys[moneys.length - 1];
 };
