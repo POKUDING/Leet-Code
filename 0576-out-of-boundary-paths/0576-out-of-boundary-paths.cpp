a2b765b851e8a4ed754db10caf26a3a01da7b3fd
JunHyung Park <yoseb95@gmail.com>
2024-01-26 14:59:59 +0900

    Time: 10 ms (42.77%), Space: 10.6 MB (23.29%) - LeetHub

---
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int dir[4] = {1, 0 , -1, 0};
+    int mod = 1000000007;
+    int cntWays(vector<vector<vector<int>>>& dp, int x, int y,int m,int n, int maxMove, int moves) {
+        int rtn = 0;
+        if (moves >= maxMove)
+            return 0;
+        if (dp[moves][y][x] != -1)
+            return dp[moves][y][x];
+        for(int i = 0; i < 4; ++i){
+            int next_x = x + dir[i];
+            int next_y = y + dir[(i + 1) % 4];
+            if (next_x < 0 || next_y < 0 || next_x >= n || next_y >= m)
+                ++rtn;
+            else
+                rtn += cntWays(dp,next_x, next_y, m, n, maxMove,moves + 1) % mod;
+            rtn %= mod;
+        }
+        dp[moves][y][x] = rtn;
+        return rtn;
+    }
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        int ans = 0;
+        vector<vector<vector<int>>> dp(maxMove, vector<vector<int>>(m, vector<int>(n, -1)));
+
+        ans = cntWays(dp, startColumn, startRow, m, n, maxMove,0);
+        return ans;
+    }
+};
