a3f3c90377c72957ea668b37e3d9014021a48559
JunHyung Park <yoseb95@gmail.com>
2024-01-27 14:00:46 +0900

    Time: 1266 ms (18.13%), Space: 11.4 MB (43.96%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -1,22 +1,19 @@
 class Solution {
- public:
-  int kInversePairs(int n, int k) {
-    constexpr int kMod = 1'000'000'007;
-    // dp[i][j] := the number of permutations of numbers 1..i with j inverse
-    // pairs
-    vector<vector<int>> dp(n + 1, vector<int>(k + 1));
+public:
+    int kInversePairs(int n, int k) {
+        int dp[1001][1001] = {
+            1,
+        };
 
-    // If there's no inverse pair, the permutation is unique "123..i".
-    for (int i = 0; i <= n; ++i)
-      dp[i][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j <= k; ++j) {
+                for (int x = 0; x <= min(j, i - 1); ++x) {
+                    if (j - x >= 0)
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - x]) % 1000000007;
+                }
+            }
+        }
 
-    for (int i = 1; i <= n; ++i)
-      for (int j = 1; j <= k; ++j) {
-        dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % kMod;
-        if (j - i >= 0)
-          dp[i][j] = (dp[i][j] - dp[i - 1][j - i] + kMod) % kMod;
-      }
-
-    return dp[n][k];
-  }
+        return dp[n][k];
+    }
 };
