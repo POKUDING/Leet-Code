7cef8658bf1146fbe2fa6f73353043fc7d9d30ef
JunHyung Park <yoseb95@gmail.com>
2024-01-28 19:58:45 +0900

    Time: 548 ms (63.64%), Space: 160.1 MB (55.77%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,37 +1,28 @@
-
-
-
 class Solution {
 public:
-    int numSubmatrixSumTarget(std::vector<std::vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int rtn = 0;
 
-        for (int row = 0; row < m; row++) {
-            for (int col = 1; col < n; col++) {
-                matrix[row][col] += matrix[row][col - 1];
-            }
-        }
+        for (int r = 0; r < row; ++r)
+            for (int c = 1; c < col; ++c)
+                matrix[r][c] += matrix[r][c - 1];
 
-        int count = 0;
-
-        for (int c1 = 0; c1 < n; c1++) {
-            for (int c2 = c1; c2 < n; c2++) {
-                std::unordered_map<int, int> map;
+        unordered_map<int, int> map;
+        for (int c1 = 0; c1 < col; ++c1) {
+            for (int c2 = c1; c2 < col; ++c2) {
+                map.clear();
                 map[0] = 1;
                 int sum = 0;
 
-                for (int row = 0; row < m; row++) {
-                    sum += matrix[row][c2] - (c1 > 0 ? matrix[row][c1 - 1] : 0);
-                    count += map[sum - target];
+                for (int r = 0; r < row; ++r) {
+                    sum += matrix[r][c2] - (c1 <= 0 ? 0 : matrix[r][c1 - 1]);
+                    rtn += map[sum - target];
                     map[sum]++;
                 }
             }
         }
-
-        return count;
+        return rtn;
     }
 };
-
-
-
