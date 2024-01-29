7409a72c41f490c08c2dad8598c5e7e804c8b9d8
JunHyung Park <yoseb95@gmail.com>
2024-01-29 20:09:49 +0900

    Time: 4 ms (8.25%), Space: 8.4 MB (11.04%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,33 +1,32 @@
 class MyQueue {
 private:
-    stack<int>  que;
-    stack<int>  push_s;
+    stack<int> que;
+    stack<int> push_s;
+
 public:
     MyQueue() {}
-    
+
     void push(int x) {
-        while(!que.empty()) {
+        while (!que.empty()) {
             push_s.push(que.top());
             que.pop();
         }
         push_s.push(x);
-        while(!push_s.empty()) {
+        while (!push_s.empty()) {
             que.push(push_s.top());
             push_s.pop();
         }
     }
-    
+
     int pop() {
         int rtn = que.top();
         que.pop();
         return rtn;
     }
-    
-    int peek() {
-        return que.top();
-    }
-    
-    bool empty() return que.empty();
+
+    int peek() { return que.top(); }
+
+    bool empty() { return que.empty(); }
 };
 
 /**
