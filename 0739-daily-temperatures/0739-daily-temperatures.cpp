f56b41a84b9509bbc393d501ec1feafbbd651ad8
JunHyung Park <yoseb95@gmail.com>
2024-01-31 22:46:58 +0900

    Time: 119 ms (91.31%), Space: 109.9 MB (6.29%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    int idx = 0;
-    map<int,int> res;
-    void cntDay(vector<int>& temps) {
-        int cur = idx++;
-        if(idx != temps.size()) {
-            while(idx < temps.size() && temps[cur] >= temps[idx])
-                cntDay(temps);
-        }
-        res[cur] = idx != temps.size()? idx - cur : 0;
-        return;
-    }
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> rtn;
-        while (idx < temperatures.size())
-            cntDay(temperatures);
-        for(int i = 0; i < temperatures.size(); ++i)
-            rtn.push_back(res[i]);
+        vector<int> rtn(temperatures.size(), 0);
+        stack<pair<int, int>>  s;
+
+        for(int i =0; i < temperatures.size(); ++i) {
+            while(!s.empty() && s.top().first < temperatures[i]) {
+                rtn[s.top().second] = i - s.top().second;
+                s.pop();
+            }
+            s.push({temperatures[i], i});
+        }
         return rtn;
     }
 };
