59a22f6cc62cd52f661c38aa02cd0fc6801adc80
JunHyung Park <yoseb95@gmail.com>
2024-02-04 16:17:04 +0900

    Time: 9 ms (72.68%), Space: 9.4 MB (21.92%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,49 +1,26 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int> Map;
-        unordered_map<char,int> cnt;
-        string rtn;
-        pair<int, int> tmp = {-1, s.size() + 1};
-        int start = -1, end;
-        int len = t.size();
+        int mp[126] = {0};
+        pair<int,int> rtn = {-1,INT_MAX};
+        int cnt = t.size();
 
-        for(int i = 0; i < t.size(); ++i) {
-            ++Map[t[i]];
-        }
-        while(++start < s.size())
-            if(Map[s[start]] > 0)
-                break;
-        end = start - 1;
-        while(true) {
-            while(len > 0 && ++end < s.size()) {
-                if(Map[s[end]] > 0) {
-                    if(Map[s[end]] > cnt[s[end]])
-                        --len;
-                    ++cnt[s[end]];
-                }
-            }
-            if (len != 0 || (tmp.first == start && tmp.second == end - start + 1))
-                break;
-            if (tmp.second > end - start + 1)
-                tmp = {start, end - start + 1};
-            while(start < end && len <= 1) {
-                if(Map[s[start]] > 0) {
-                    if(Map[s[start]] < cnt[s[start]])
-                        --cnt[s[start]];
-                    else if(Map[s[start]] == cnt[s[start]] && len == 0) {
-                        ++len;
-                        --cnt[s[start]];
-                    } else
-                        break;
-                }
+        for(int i = 0; i < t.size(); ++i)
+            ++mp[t[i]];
+        
+        for(int end = 0, start = 0; end < s.size(); ++end) {
+            if (--mp[s[end]] >= 0)
+                --cnt;
+            while(cnt == 0) {
+                if(end - start + 1 < rtn.second)
+                    rtn = {start, end - start + 1};
+                if (++mp[s[start]] > 0)
+                    ++cnt;
                 ++start;
-                if (len == 0 && tmp.second > end - start + 1)
-                    tmp = {start, end - start + 1};
             }
         }
-        if(tmp.first == -1)
+        if (rtn.second == INT_MAX)
             return "";
-        return s.substr(tmp.first, tmp.second);
+        return s.substr(rtn.first, rtn.second);
     }
 };
