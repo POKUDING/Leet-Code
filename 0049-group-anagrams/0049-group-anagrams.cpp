1dfe83368d6c14ef1dca2f43dd7f1f6c4532c4c9
JunHyung Park <yoseb95@gmail.com>
2024-02-06 12:51:13 +0900

    Time: 23 ms (89.86%), Space: 23.4 MB (32.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        cin.tie(0);
+        ios::sync_with_stdio(0);
         map<string,vector<string>> memo;
         vector<vector<string>> rtn;
 
-        for(int i = 0; i < strs.size(); ++i) {
-            string tmp(strs[i]);
+        for(auto it = strs.begin(); it != strs.end(); ++it) {
+            string tmp(*it);
             sort(tmp.begin(), tmp.end());
-            memo[tmp].push_back(strs[i]);
+            memo[tmp].push_back(*it);
         }
-        for(map<string,vector<string>>::iterator it = memo.begin(); it != memo.end(); ++it)
+        for(auto it = memo.begin(); it != memo.end(); ++it)
             rtn.push_back(it->second);
         return rtn;
     }
