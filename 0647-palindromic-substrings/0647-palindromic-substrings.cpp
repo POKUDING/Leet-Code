5448fe2ab12ed00f16f7b1912905980b6a018c12
JunHyung Park <yoseb95@gmail.com>
2024-02-10 14:20:56 +0900

    Time: 264 ms (12.5%), Space: 7.5 MB (74.15%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int cnt(int s, int e, string& str) {
+        int rtn = 0;
+        while(s >= 0 && e < str.size()) {
+            if(str[s--] == str[e++]) {
+                ++rtn;
+                cout << s + 1 << " " << e - 1 <<"\n";
+            } else 
+                break;
+        }
+        return rtn;
+    }
+    int countSubstrings(string s) {
+        int ans = 0;
+        for(int i = 0; i < s.size(); ++i) {
+            ans += cnt(i, i, s);
+            ans += cnt(i, i + 1, s);
+        }
+        return ans;
+    }
+};
