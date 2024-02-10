25ab6fe42ca2bfc39eccb2de894a32abe4d0087c
JunHyung Park <yoseb95@gmail.com>
2024-02-10 20:39:18 +0900

    Time: 10 ms (97.32%), Space: 11.3 MB (32.74%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -6,20 +6,20 @@ public:
         int max_idx = 0;
         vector<int> dp(len, 0), before(len), ans;
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < len; ++i) {
+        for (int i = 0; i < len; ++i) {
             before[i] = -1;
-            for(int j = 0; j < i; ++j) {
-                if(nums[i] % nums[j] == 0 && dp[j] > dp[i]) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] % nums[j] == 0 && dp[j] > dp[i]) {
                     dp[i] = dp[j];
                     before[i] = j;
                 }
             }
-            if(max_dp < ++dp[i]) {
+            if (max_dp < ++dp[i]) {
                 max_idx = i;
                 max_dp = dp[i];
             }
         }
-        while(max_idx != -1) {
+        while (max_idx != -1) {
             ans.push_back(nums[max_idx]);
             max_idx = before[max_idx];
         }
