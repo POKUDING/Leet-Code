9818a6cce515869638b2553e4d7b7a414ee45101
JunHyung Park <yoseb95@gmail.com>
2024-02-12 16:20:49 +0900

    Time: 18 ms (29.34%), Space: 22 MB (33.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,9 +3,10 @@ public:
     int majorityElement(vector<int>& nums) {
         int cur = 0, cnt = 0;
         for(int i = 0; i < nums.size(); ++i) {
-            if (cnt == 0)
+            if (cnt == 0) {
                 cur = nums[i];
-            else if (cur == nums[i])
+                ++cnt;
+            } else if (cur == nums[i])
                 ++cnt;
             else
                 --cnt;
