192b5baed14816638c6efd97e814203f31582ecf
JunHyung Park <yoseb95@gmail.com>
2024-02-21 14:49:36 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,3 +1,4 @@
+static auto _ = []() { std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr); return 
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
