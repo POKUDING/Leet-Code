eda90f19f94e40e12aa221c74e29cc578e4d9bc3
JunHyung Park <yoseb95@gmail.com>
2024-02-23 15:01:37 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<vector<int>> memo(n + 1, vector<int>(n + 1));
-        int mindep = INT_MAX;
         queue<tuple<int,int, int>> que;
 
         for (int i = 0; i < flights.size(); ++i) {
@@ -11,40 +10,17 @@ public:
             if (flights[i][1] != src)
                memo[flights[i][0]][flights[i][1]] = flights[i][2];
         }
-        // for (int i = 0; i <=n ; ++i) {
-        //     for(int j = 0; j <= n; ++j)
-        //         cout << memo[i][j] << " ";
-        //     cout << "\n";
-        // }
-        // cout << "===================\n";
         while(!que.empty()) {
             auto [cur, dep, cost] = que.front();
-            cout << cur << " " << dep << " "<< cost <<"\n";
             que.pop();
-            if(cur == dst || dep >= k)
-                continue;
             for (int i = 0; i <= n; ++i) {
                 if(memo[cur][i] == 0 || ( memo[src][i] && memo[src][i] <= cost + memo[cur][i]))
                     continue;
-                // cout << "===================\n";
-                // cout << cur << " " << i << "\n";
-                // for (int i = 0; i <=n ; ++i) {
-                //     for(int j = 0; j <= n; ++j)
-                //         cout << memo[i][j] << " ";
-                //     cout << "\n";
-                // }
-                // cout << "===================\n";
                 memo[src][i] = cost + memo[cur][i];
-                que.push(make_tuple(i, dep + 1, memo[src][i]));
+                if(i != dst && dep < k)
+                    que.push(make_tuple(i, dep + 1, memo[src][i]));
             }
         }
-        // cout << "===================\n";
-        // for (int i = 0; i <=n ; ++i) {
-        //     for(int j = 0; j <= n; ++j)
-        //         cout << memo[i][j] << " ";
-        //     cout << "\n";
-        // }
-        // cout << "===================\n";
         return memo[src][dst] > 0 ? memo[src][dst] : -1;
     }
 };
