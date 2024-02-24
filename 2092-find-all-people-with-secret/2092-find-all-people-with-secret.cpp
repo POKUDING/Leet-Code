ef9979d5dc29c77ef72ce31b12204e811d5c0b2c
JunHyung Park <yoseb95@gmail.com>
2024-02-24 13:59:39 +0900

    Time: 370 ms (94.67%), Space: 163.8 MB (77.87%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        vector<pair<int,int>>graph[n];
-        vector<int>result;
-        for(int i=0;i<meetings.size();i++){
-            graph[meetings[i][0]].push_back(make_pair(meetings[i][1],meetings[i][2]));
-            graph[meetings[i][1]].push_back(make_pair(meetings[i][0],meetings[i][2]));
+        queue<int> que;
+        vector<int> know(n, -1);
+        vector<int> rtn;
+        vector<pair<int,int>> memo[n];
+
+        for(int i = 0; i < meetings.size(); ++i) {
+            memo[meetings[i][0]].push_back(make_pair(meetings[i][1], meetings[i][2]));
+            memo[meetings[i][1]].push_back(make_pair(meetings[i][0], meetings[i][2]));
         }
-        queue<int>q;
-        vector<int>infoTime(n,-1);
-        q.push(0);
-        q.push(firstPerson);
-        infoTime[0]=0;
-        infoTime[firstPerson]=0;
-        while(q.size()!=0){
-            int f=q.front();
-            q.pop();
-            for(int i=0;i<graph[f].size();i++){
-                int child=graph[f][i].first;
-                int time=graph[f][i].second;
-                if((infoTime[child]==-1 && infoTime[f]<=time) ||
-                  (infoTime[child]!=-1 && infoTime[f]<=time && time<infoTime[child])){
-                      infoTime[child]=time;
-                      q.push(child);
-                  }
+        que.push(0);
+        que.push(firstPerson);
+        know[0] = 0;
+        know[firstPerson] = 0;
+        while(!que.empty()) {
+            int from = que.front();
+            int time = know[from];
+            que.pop();
+            for(int i = 0; i < memo[from].size(); ++i) {
+                int met = memo[from][i].first;
+                int mettime = memo[from][i].second;
+                if((know[met] == -1 || know[met] > mettime) && mettime >= time) {
+                    know[met] = mettime;
+                    que.push(met);
+                }
             }
         }
-        for(int i=0;i<infoTime.size();i++){
-            if(infoTime[i]!=-1)
-                result.push_back(i);
+        for(int i = 0; i < n; ++i) {
+            if(know[i] != -1)
+                rtn.push_back(i);
         }
-        return result;
+        return rtn;
     }
 };
