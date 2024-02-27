01b6e6fe53f89db8d096ba317625b59e5eb8a368
JunHyung Park <yoseb95@gmail.com>
2024-02-27 14:03:04 +0900

    Time: 3 ms (95.23%), Space: 18.9 MB (90.86%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,7 +1,6 @@
-
+const static auto _ = [] { std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr); return nullptr; }();
 class Solution {
 public:
-    // int maxlen = 0;
     int dfs(TreeNode* root, int& maxlen) {
         if (!root)
             return -1;
