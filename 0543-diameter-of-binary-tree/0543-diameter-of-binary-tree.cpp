098b23a1b7bb69ee99faf71b9badc31531adf6a5
JunHyung Park <yoseb95@gmail.com>
2024-02-27 13:50:46 +0900

    Time: 9 ms (47.78%), Space: 19 MB (70.17%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,17 +11,18 @@
  */
 class Solution {
 public:
-    int maxlen = 0;
-    int dfs(TreeNode* root) {
+    // int maxlen = 0;
+    int dfs(TreeNode* root, int& maxlen) {
         if(!root)
             return -1;
-        int left_len = dfs(root->left) + 1;
-        int right_len = dfs(root->right) + 1;
+        int left_len = dfs(root->left, maxlen) + 1;
+        int right_len = dfs(root->right, maxlen) + 1;
         maxlen = max(maxlen, left_len + right_len);
         return max(left_len, right_len);
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        dfs(root);
+        int maxlen = 0;
+        dfs(root, maxlen);
         return maxlen;
     }
 };
