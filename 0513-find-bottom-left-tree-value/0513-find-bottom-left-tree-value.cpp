0e7b9d9e93c1266ea3edb64b03196719513baa5a
JunHyung Park <yoseb95@gmail.com>
2024-02-28 17:31:35 +0900

    Time: 8 ms (65.28%), Space: 20.5 MB (47.81%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -2,19 +2,14 @@ const static auto _ = [](){cin.tie(0); cout.tie(0); ios::sync_with_stdio(0); ret
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-        queue<pair<int,TreeNode*>> que;
-        pair<int,int> rtn = make_pair(-1,0);
-        que.push(make_pair(0, root));
+        queue<TreeNode*> que(deque<TreeNode*>(1,root));
+        TreeNode* curr;
         while(!que.empty()) {
-            auto [dep, head] = que.front();
+            curr = que.front();
             que.pop();
-            if(head == nullptr)
-                continue;
-            if(dep > rtn.first)
-                rtn = make_pair(dep, head->val);
-            que.push(make_pair(dep + 1, head->left));
-            que.push(make_pair(dep + 1, head->right));
+            if(curr->right) que.push(curr->right);
+            if(curr->left) que.push(curr->left);
         }
-        return rtn.second;
+        return curr->val;
     }
 };
