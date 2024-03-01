c4fa3208fd31c73c68e7d63b019c3f6e03d352b6
JunHyung Park <yoseb95@gmail.com>
2024-03-01 12:22:53 +0900

    Time: 3 ms (77.44%), Space: 9 MB (19.34%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -2,13 +2,10 @@ const static auto _ = [](){cin.tie(0); cout.tie(0); ios::sync_with_stdio(0); ret
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        int cnt0 = 0;
+        int cnt0 = count(begin(s),end(s), '0');
         int len = s.size();
         string rtn;
-        for(int i = 0; i < len; ++i) {
-            if(s[i] == '0')
-                ++cnt0;
-        }
+
         while((len--) - cnt0 > 1)
             rtn += '1';
         while(cnt0-- > 0)
