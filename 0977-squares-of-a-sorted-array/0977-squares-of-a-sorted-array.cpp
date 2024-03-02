660f77fd18fb0a1cef8ed5953cb6dfec901d5689
JunHyung Park <yoseb95@gmail.com>
2024-03-02 11:35:06 +0900

    Time: 24 ms (58.03%), Space: 28.3 MB (58.32%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -2,8 +2,9 @@ const static auto _ = [](){cin.tie(0); cout.tie(0); ios::sync_with_stdio(0); ret
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> rtn(nums.size());
-        for(int i = nums.size() - 1, s = 0, e = i; i >= 0; --i){
+        int size = nums.size();
+        vector<int> rtn(size);
+        for(int i = size - 1, s = 0, e = i; i >= 0; --i){
             if(abs(nums[s]) >= abs(nums[e]))
                 rtn[i] = pow(nums[s++],2);
             else
