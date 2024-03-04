bdd092ca92911db0b195841e8600e2dfdf4eb76e
JunHyung Park <yoseb95@gmail.com>
2024-03-04 10:16:46 +0900

    Time: 0 ms (100%), Space: 13.2 MB (23.53%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -1,4 +1,4 @@
-const static auto _ = [](){cin.tie(0); cout.tioe(0); ios::sync_with_stdio(0); return nullptr;}();
+const static auto _ = [](){cin.tie(0); cout.tie(0); ios::sync_with_stdio(0); return nullptr;}();
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
