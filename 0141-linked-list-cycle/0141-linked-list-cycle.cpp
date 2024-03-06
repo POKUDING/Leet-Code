830bc32557539b882c9cfb932bcb292053e19cc2
JunHyung Park <yoseb95@gmail.com>
2024-03-06 11:55:06 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -2,10 +2,12 @@ const static auto _ = []{cin.tie(0); cout.tie(0); ios::sync_with_stdio(0); retur
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        while(head) {
-            if(head->val == INT_MAX) return true;
-            head->val = INT_MAX;
-            head = head->next;
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast -> next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast) return true;
         }
         return false;
     }
