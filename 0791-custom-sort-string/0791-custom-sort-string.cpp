3604df3c57007165b9baed1a926122e68c5e8158
JunHyung Park <yoseb95@gmail.com>
2024-03-11 12:33:38 +0900

    Time: 0 ms (100%), Space: 9.5 MB (6.35%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<char,int> pos;
         for(int i = 0; i < order.size(); ++i)
             pos[order[i]] = i + 1;
-        sort(s.begin(),s.end(),[&pos](char s1, char s2){return pos[s1] < pos[s2];});
+        sort(s.begin(),s.end(),[&](char s1, char s2){return pos[s1] < pos[s2];});
         return s;
     }
 };
