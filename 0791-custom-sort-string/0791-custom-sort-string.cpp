e16bdfbd22fad666d58cb92dc1a6327a8a51ad30
JunHyung Park <yoseb95@gmail.com>
2024-03-11 12:27:10 +0900

    Time: 5 ms (8.56%), Space: 9.4 MB (8.68%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        unordered_map<char,int> pos;
+        for(int i = 0; i < order.size(); ++i)
+            pos[order[i]] = i + 1;
+        sort(s.begin(),s.end(),[&pos](char s1, char s2){return pos[s1] < pos[s2];});
+        return s;
+    }
+};
