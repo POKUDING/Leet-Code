760aa741322329720419b3c5acacc8f97b83226e
JunHyung Park <yoseb95@gmail.com>
2024-03-13 12:54:07 +0900

    Time: 0 ms (100%), Space: 7.4 MB (15.68%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     int pivotInteger(int n) {
         double ans = sqrt(n*(n+1) / 2);
-        return ans == int(ans) ? ans : -1;
+        return ans == floor(ans) ? ans : -1;
     }
 };
