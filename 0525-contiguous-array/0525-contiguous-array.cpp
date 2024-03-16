0a89674246fabf5ed93333f68e6be46935bf49aa
JunHyung Park <yoseb95@gmail.com>
2024-03-16 13:02:20 +0900

    Time: 48 ms (99.71%), Space: 88.4 MB (14.84%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -2,17 +2,18 @@ const static auto _ = []() {cin.tie(0); cout.tie(0); ios::sync_with_stdio(0); re
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
-        unordered_map<int,int> mp;
+        vector<int> mp(nums.size() * 2 + 1);
+        int n = nums.size();
         int sum = 0;
         int max_len = 0;
         for(int i = 0; i < nums.size(); ++i) {
             sum += nums[i] == 0 ? 1 : -1;
             if(sum == 0)
                 max_len = i + 1;
-            if(mp[sum] != 0)
-                max_len = max(max_len, i - mp[sum] + 1);
+            if(mp[sum + n] != 0)
+                max_len = max(max_len, i - mp[sum + n] + 1);
             else
-                mp[sum] = i + 1;
+                mp[sum + n] = i + 1;
         }
         return max_len;
     }
