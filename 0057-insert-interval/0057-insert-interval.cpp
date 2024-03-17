f315f0658a934a0a6a86e5b0293951b071324ba3
JunHyung Park <yoseb95@gmail.com>
2024-03-17 18:45:09 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -3,13 +3,9 @@ public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> rtn;
         int i = 0;
-        for(;i < intervals.size(); ++i) {
-            if(intervals[i][1] < newInterval[0]) {
-                rtn.push_back(intervals[i]);
-                continue;
-            }
-            if (intervals[i][0] > newInterval[1])
-                break;
+        for(;i < intervals.size() && intervals[i][1] < newInterval[0]; ++i)
+            rtn.push_back(intervals[i]);
+        for(;i < intervals.size() && intervals[i][0] <= newInterval[1]) {
             newInterval[0] = min(newInterval[0], intervals[i][0]);
             newInterval[1] = max(newInterval[1], intervals[i][1]);
         }
