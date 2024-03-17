a39282e7c94bca5367bdc9dbaece5f35a7c3f6da
JunHyung Park <yoseb95@gmail.com>
2024-03-17 18:45:26 +0900

    Time: 7 ms (92.69%), Space: 20.5 MB (58.08%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -5,7 +5,7 @@ public:
         int i = 0;
         for(;i < intervals.size() && intervals[i][1] < newInterval[0]; ++i)
             rtn.push_back(intervals[i]);
-        for(;i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+        for(;i < intervals.size() && intervals[i][0] <= newInterval[1]; ++i) {
             newInterval[0] = min(newInterval[0], intervals[i][0]);
             newInterval[1] = max(newInterval[1], intervals[i][1]);
         }
