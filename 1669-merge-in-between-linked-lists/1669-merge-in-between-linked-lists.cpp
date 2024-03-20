03ec401bd52a4f446d3821b5b83a36a6f6e5fe9c
JunHyung Park <yoseb95@gmail.com>
2024-03-20 11:24:43 +0900

    Time: 133 ms (99.94%), Space: 98.1 MB (44.8%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
@@ -8,6 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+const static auto _ = [](){cin.tie(0); cout.tie(0); ios::sync_with_stdio(0); return nullptr; }();
 class Solution {
 public:
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
