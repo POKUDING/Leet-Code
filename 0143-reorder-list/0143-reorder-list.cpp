ac00945789856fb381ba8c8e04f52ec085950e3b
JunHyung Park <yoseb95@gmail.com>
2024-03-23 11:35:32 +0900

    Time: 16 ms (96.5%), Space: 21.1 MB (99.47%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -8,25 +8,54 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     void reorderList(ListNode* head) {
+//         vector<ListNode *> arr;
+//         ListNode* tmp = head;
+//         while(tmp) {
+//             arr.push_back(tmp);
+//             tmp = tmp->next;
+//         }
+//         tmp = head;
+//         int s = 1, e = arr.size() - 1;
+//         while(s <= e) {
+//             tmp->next = arr[e--];
+//             tmp = tmp->next;
+//             if(s <= e) {
+//                 tmp->next = arr[s++];
+//                 tmp = tmp->next;
+//             }
+//         }
+//         tmp->next = nullptr;
+//     }
+// };
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        vector<ListNode *> arr;
-        ListNode* tmp = head;
-        while(tmp) {
-            arr.push_back(tmp);
-            tmp = tmp->next;
+        ListNode* slow = head, *fast = head;
+        while(fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        tmp = head;
-        int s = 1, e = arr.size() - 1;
-        while(s <= e) {
-            tmp->next = arr[e--];
-            tmp = tmp->next;
-            if(s <= e) {
-                tmp->next = arr[s++];
-                tmp = tmp->next;
-            }
+        ListNode *curr = slow->next, *prev = nullptr, *next;
+        while(curr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr =next;
         }
-        tmp->next = nullptr;
+        slow->next = nullptr;
+        ListNode *front = head, *back = prev;
+        while(front && back) {
+            ListNode *front_next = front->next;
+            ListNode *back_next = back->next;
+
+            front->next = back;
+            back->next = front_next;
+
+            front = front_next;
+            back = back_next;
+        }
     }
 };
