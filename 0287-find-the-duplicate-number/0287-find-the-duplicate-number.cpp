31de6073593b84ee128a498765582dd652cca573
JunHyung Park <yoseb95@gmail.com>
2024-03-24 20:36:33 +0900

    Time: 110 ms (28.68%), Space: 64.7 MB (42.58%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        int visit[100001] = {0};
-        for(int i = 0, size = nums.size(); i < size; ++i)
-        {
-          if (visit[nums[i]])
-            return nums[i];
-          visit[nums[i]] = 1;
-        }
-        return -1;
+        sort(nums.begin(),nums.end());
+        for(int i = 1; i < nums.size(); ++i)
+            if(nums[i] == nums[i - 1])
+                return nums[i];
+        return 0;
     }
 };
