40583cf8b4d66f3b5d796c0005372b8f1683d31c
JunHyung Park <yoseb95@gmail.com>
2024-03-26 12:10:37 +0900

    Time: 39 ms (80.04%), Space: 43.7 MB (55.21%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,18 +1,18 @@
-// class Solution {
-// public:
-//     int firstMissingPositive(vector<int>& nums) {
-//         for(int i = 0; i < nums.size(); ++i)
-//             if(nums[i] <= 0)
-//                 nums[i] = INT_MAX;
-//         for(int i = 0; i < nums.size(); ++i)
-//             if(abs(nums[i]) - 1 < nums.size() && nums[abs(nums[i]) - 1] > 0)
-//                 nums[abs(nums[i]) - 1] *= -1;
-//         for(int i = 0 ; i < nums.size(); ++i)
-//             if(nums[i] >= 0)
-//                 return i + 1;
-//         return nums.size() + 1;
-//     }
-// };
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); ++i)
+            if(nums[i] <= 0)
+                nums[i] = INT_MAX;
+        for(int i = 0; i < nums.size(); ++i)
+            if(abs(nums[i]) - 1 < nums.size() && nums[abs(nums[i]) - 1] > 0)
+                nums[abs(nums[i]) - 1] *= -1;
+        for(int i = 0 ; i < nums.size(); ++i)
+            if(nums[i] >= 0)
+                return i + 1;
+        return nums.size() + 1;
+    }
+};
 
 // class Solution:
 //     def firstMissingPositive(self, nums: List[int]) -> int:
@@ -28,21 +28,21 @@
 //                 return i + 1
 //         return N + 1
 
-class Solution {
-public:
-    int firstMissingPositive(vector<int>& nums) {
-        int N = nums.size();
-        for(int i = 0; i < N; ++i) {
-            while(0 < nums[i] && nums[i] <= N && nums[nums[i] - 1] != nums[i]) {
-                int idx = nums[i] - 1;
-                int tmp = nums[idx];
-                nums[idx] = nums[i];
-                nums[i] = tmp;
-            }
-        }
-        for(int i = 0; i < N; ++i)
-            if(nums[i] != i + 1)
-                return i + 1;
-        return N + 1;
-    }
-};
+// class Solution {
+// public:
+//     int firstMissingPositive(vector<int>& nums) {
+//         int N = nums.size();
+//         for(int i = 0; i < N; ++i) {
+//             while(0 < nums[i] && nums[i] <= N && nums[nums[i] - 1] != nums[i]) {
+//                 int idx = nums[i] - 1;
+//                 int tmp = nums[idx];
+//                 nums[idx] = nums[i];
+//                 nums[i] = tmp;
+//             }
+//         }
+//         for(int i = 0; i < N; ++i)
+//             if(nums[i] != i + 1)
+//                 return i + 1;
+//         return N + 1;
+//     }
+// };
