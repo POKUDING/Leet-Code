a3582c7a6719fb201deb0a5434cc54908c5efd53
JunHyung Park <yoseb95@gmail.com>
2024-04-02 12:48:42 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int len = s.length();
+        Map<Character, Character> map = new HashMap<Character,Character>();
+        for(int i = 0; i < len; ++i){
+            if(map.containsKey(s.charAt(i))) {
+                if(map.get(s.charAt(i)) != t.charAt(i))
+                    return false;
+            } else
+                map.put(s.charAt(i), t.charAt(i));
+        }
+        return true;
+    }
+}
