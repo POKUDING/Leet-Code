ae36fdd31fa434d7f30813a1526f1f644583e0d1
JunHyung Park <yoseb95@gmail.com>
2024-04-03 11:36:59 +0900

    Time: 135 ms (76.29%), Space: 41.5 MB (49.08%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,36 +1,34 @@
 class Solution {
-    int x_size, y_size;
-    boolean[][] visit;
-    int[] dirX = {0, 1, 0, -1};
-    int[] dirY = {1, 0, -1, 0};
-    public boolean exist(char[][] board, String word) {
-        y_size = board.length;
-        x_size = board[0].length;
-        visit = new boolean[y_size][x_size];
-        char[] charArr = word.toCharArray();
+    private boolean helper(char[][] arr, String word, int i, int j, int idx, int row, int col, int[][] visited) {
+        if (i < 0 || j < 0 || i >= row || j >= col || visited[i][j] == 1)
+            return false;
+        if (word.charAt(idx) != arr[i][j])
+            return false;
+        if (idx == word.length() - 1)
+            return true;
 
-        for(int i = 0; i < y_size; ++i) {
-            for(int j = 0; j < x_size; ++j) {
-                if(board[i][j] == charArr[0] && dfs(i,j,0,board,charArr))
-                    return true;
-            }
-        }
+        visited[i][j] = 1;
+        if (helper(arr, word, i, j + 1, idx + 1, row, col, visited) ||
+            helper(arr, word, i + 1, j, idx + 1, row, col, visited) ||
+            helper(arr, word, i - 1, j, idx + 1, row, col, visited) ||
+            helper(arr, word, i, j - 1, idx + 1, row, col, visited))
+            return true;
+        
+        visited[i][j] = 0;
         return false;
     }
 
-    boolean dfs(int y, int x, int dep, char[][] board, char[] charArr) {
-        if(dep == charArr.length)
-            return true;
-        if(y < 0 || x < 0 || y >= y_size || x >= x_size)
-            return false;
-        if(visit[y][x] == true || board[y][x] != charArr[dep])
-            return false;
-        visit[y][x] = true;
-        for(int i = 0; i < 4; ++i) {
-            if(dfs(y + dirY[i], x + dirX[i], dep + 1, board, charArr))
-                return true;
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+        int[][] visited = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (helper(board, word, i, j, 0, row, col, visited))
+                    return true;
+            }
         }
-        visit[y][x] = false;
         return false;
     }
 }
