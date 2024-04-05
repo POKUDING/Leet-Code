16b65e1bdb7060f5e139012e6813e4813f8ed39a
JunHyung Park <yoseb95@gmail.com>
2024-04-05 13:05:11 +0900

    Time: 2 ms (88.33%), Space: 41.2 MB (99.73%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -3,9 +3,10 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         sb.append(s.charAt(0));
         for(int i = 1; i < s.length(); ++i) {
-            if(sb.length() != 0 && sb.charAt(sb.length() - 1) != s.charAt(i) &&
-                Character.toLowerCase(sb.charAt(sb.length() - 1)) == Character.toLowerCase(s.charAt(i)))
-                sb.setLength(sb.length() - 1);
+            int len = sb.length();
+            if(len != 0 && sb.charAt(len - 1) != s.charAt(i) &&
+                Character.toLowerCase(sb.charAt(len - 1)) == Character.toLowerCase(s.charAt(i)))
+                sb.setLength(len - 1);
             else
                 sb.append(s.charAt(i));
         }
