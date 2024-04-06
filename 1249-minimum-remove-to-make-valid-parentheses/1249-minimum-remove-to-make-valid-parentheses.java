fed1f746d48b83df226521691c7c8de78fddecee
JunHyung Park <yoseb95@gmail.com>
2024-04-06 15:40:03 +0900

    Time: 20 ms (60.87%), Space: 45.5 MB (41.4%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < s.length(); ++i) {
+            if(s.charAt(i) == '(')
+                arr.add(i);
+            else if (s.charAt(i) == ')') {
+                if(arr.size() != 0 && s.charAt(arr.get(arr.size() - 1)) == '(')
+                    arr.remove(arr.size() - 1);
+                else
+                    arr.add(i);
+            }
+        }
+        for(int i = 0, j = 0; i < s.length(); ++i) {
+            if(j < arr.size() && arr.get(j) == i)
+                ++j;
+            else
+                sb.append(s.charAt(i));
+
+        }
+        return sb.toString();
+    }
+}
