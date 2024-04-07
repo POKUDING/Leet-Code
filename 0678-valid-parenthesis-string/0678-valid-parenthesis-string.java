7b565f2716467668066631740e3eef1155664eae
JunHyung Park <yoseb95@gmail.com>
2024-04-07 13:37:33 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int unusedStar = 0, leftStar = 0, rightStar = 0;
+        int left = 0, right = 0;
+        for(int i = 0; i < s.length(); ++i) {
+            if(s.charAt(i) == '*')
+                ++unusedStar;
+            else if (s.charAt(i) == '(')
+                ++left;
+            else if (s.charAt(i) == ')')
+                ++right;
+
+            if (left != 0 && unusedStar != 0) {
+                --left;
+                --unusedStar;
+                ++rightStar;
+            }
+            if (right != 0) {
+                if(left != 0)
+                    --left;
+                else if (rightStar != 0) {
+                    --rightStar;
+                    ++unusedStar;
+                } else if (unusedStar != 0) {
+                    --unusedStar;
+                    ++leftStar;
+                } else {
+                    System.out.println(i);
+                    return false;
+                }
+                --right;
+            }
+            // System.out.println(s.charAt(i) + " " + left + " " + leftStar + " " + rightStar + " " + unusedStar);
+        }
+        return left != 0 ? false : true;
+    }
+}
