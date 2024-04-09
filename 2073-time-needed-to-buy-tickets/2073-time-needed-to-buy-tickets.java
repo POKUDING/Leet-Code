0f2d563e265f4e5c122f49b06fe282e2e90fc449
JunHyung Park <yoseb95@gmail.com>
2024-04-09 10:05:00 +0900

    Time: 0 ms (100%), Space: 41.1 MB (67.71%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.java
@@ -1,21 +1,12 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        int leftPeople = tickets.length;
         int time = 0;
         for(int i = 0; i < tickets.length; ++i) {
-            if(map.containsKey(tickets[i]))
-                map.put(tickets[i],map.get(tickets[i]) + 1);
+            if(i <= k)
+                time += Math.min(tickets[k], tickets[i]);
             else
-                map.put(tickets[i], 1);
+                time += Math.min(tickets[k] - 1, tickets[i]);
         }
-        for(int i = 0; i < tickets[k]; ++i) {
-            leftPeople -= map.get(i) == null ? 0 : map.get(i);
-            time += leftPeople;
-        }
-        for(int i = k + 1; i <tickets.length; ++i)
-            if(tickets[i] >= tickets[k])
-                --time;
         return time;
     }
 }
