0c8805a54ee95ba5e62776ff48e9c1d34b969134
JunHyung Park <yoseb95@gmail.com>
2024-04-21 16:46:06 +0900

    Time: 2 ms (100%), Space: 108.9 MB (96.08%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,33 +1,22 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         boolean[] visit = new boolean[n];
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
-        for(int i = 0; i < n; ++i)
-            arr.add(new ArrayList<Integer>());
-        for(int i = 0; i < edges.length; ++i) {
-            arr.get(edges[i][0]).add(edges[i][1]);
-            arr.get(edges[i][1]).add(edges[i][0]);
-        }
+
         visit[source] = true;
-        return bfs(visit, arr, source, destination);
-    }
-
-    public boolean bfs(boolean[] visit, ArrayList<ArrayList<Integer>> arr, int start, int dest) {
-        Deque<Integer> que = new ArrayDeque<>();
-
-        que.add(start);
-        while(!que.isEmpty()) {
-            int curr = que.poll();
-            if(curr == dest)
-                return true;
-            for(int i = 0; i < arr.get(curr).size(); ++i) {
-                int next = arr.get(curr).get(i);
-                if(!visit[next]) {
-                    visit[next] = true;
-                    que.add(next);
+        while(true) {
+            boolean flag = true;
+            for(int[] edge : edges) {
+                if(visit[edge[0]] != visit[edge[1]]) {
+                    visit[edge[0]] = true;
+                    visit[edge[1]] = true;
+                    flag = false;
                 }
             }
+            if(visit[destination])
+                return true;
+            if(flag)
+                return false;
         }
-        return false;
+        // return false;
     }
 }
