21573abf897b3b1d786056c660bda3f524ee5265
JunHyung Park <yoseb95@gmail.com>
2024-04-26 10:25:37 +0900

    Time: 3 ms (94.22%), Space: 49.5 MB (78.23%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -2,21 +2,32 @@ class Solution {
     public int minFallingPathSum(int[][] grid) {
         int height = grid.length;
         int width = grid[0].length;
+        int Max = Integer.MAX_VALUE;
+        int prevMin = 0, prevSecondMin = 0, prevMinIdx = 0;
+        int currMin = 0, currSecondMin = 0, currMinIdx = 0;
 
-        for(int row = 1; row < height; ++row) {
+        for(int row = 0; row < height; ++row) {
+            prevMin = currMin;
+            prevSecondMin = currSecondMin;
+            prevMinIdx = currMinIdx;
+            currMin = Max;
+            currSecondMin = Max;
             for(int col = 0; col < width; ++col) {
-                int currNum = grid[row][col];
-                grid[row][col] = Integer.MAX_VALUE;
-                for(int prevCol = 0; prevCol < width; ++prevCol) {
-                    if(prevCol == col) continue;
-                    grid[row][col] = Math.min(grid[row][col], currNum + grid[row - 1][prevCol]);
+                // System.out.println(grid[row][col] + " " + col + " " + prevMinIdx + " " +prevSecondMin + " " + prevMin);
+                grid[row][col] += (col == prevMinIdx ? prevSecondMin : prevMin);
+                // System.out.println(grid[row][col]);
+                if(grid[row][col] < currMin) {
+                    currSecondMin = currMin;
+                    currMin = grid[row][col];
+                    currMinIdx = col;
+                } else if(grid[row][col] < currSecondMin) {
+                    currSecondMin = grid[row][col];
                 }
             }
         }
-        int ans = Integer.MAX_VALUE;
         for(int col = 0; col < width; ++col) {
-            ans = Math.min(ans, grid[height - 1][col]);
+            Max = Math.min(Max, grid[height - 1][col]);
         }
-        return ans;
+        return Max;
     }
 }
