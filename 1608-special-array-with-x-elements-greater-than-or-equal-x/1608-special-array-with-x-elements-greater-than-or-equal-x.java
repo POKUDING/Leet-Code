f6cf63a314f52e84a70d95d3462755b45959c35c
JunHyung Park <yoseb95@gmail.com>
2024-05-27 10:36:30 +0900

    Time: 0 ms (100%), Space: 41 MB (83.2%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,20 +1,19 @@
 class Solution {
     public int specialArray(int[] nums) {
-        Arrays.sort(nums);
-        int sIdx = 0;
-        int eIdx = nums.length;
-
-        while(sIdx <= eIdx) {
-            int mid = (sIdx + eIdx) / 2;
-            int currIdx = nums.length - mid;
-            // System.out.println(sIdx + " " + eIdx + " " + mid + " " + currIdx);
-            if ((mid == 0 || nums[currIdx] >= mid) && (currIdx - 1 == -1 || nums[currIdx - 1] < mid))
-                return mid;
-            if(currIdx >= nums.length || nums[currIdx] > mid)
-                sIdx = mid + 1;
+        int[] counts = new int[nums.length + 1];
+        for(int num : nums) {
+            if(num > nums.length)
+                ++counts[nums.length];
             else
-                eIdx = mid - 1;
-            // System.out.println(sIdx + " " + eIdx + " " + mid + " " + currIdx +"\n");
+                ++counts[num];
+        }
+        int addCount = 0;
+        for(int i = counts.length - 1; i >= 0; --i) {
+            addCount += counts[i];
+            if(addCount > i)
+                return -1;
+            if(addCount == i)
+                return i;
         }
         return -1;
     }
