0c9bdeba695fdd931dee359f6754dedb4623c49e
JunHyung Park <yoseb95@gmail.com>
2024-05-28 10:48:53 +0900

    Time: 7 ms (73.45%), Space: 43.6 MB (12.42%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -4,8 +4,6 @@ class Solution {
         int rtn = 0;
 
         prefixSum[1] = Math.abs(s.charAt(0) - t.charAt(0));
-        for(int i = 1; i < s.length(); ++i)
-            prefixSum[i + 1] = prefixSum[i] + Math.abs(s.charAt(i) - t.charAt(i));
         for(int i = 0, j = 1; j < prefixSum.length; ++j) {
             prefixSum[j] = prefixSum[j - 1] + Math.abs(s.charAt(j - 1) - t.charAt(j - 1));
             while(prefixSum[j] - prefixSum[i] > maxCost)
