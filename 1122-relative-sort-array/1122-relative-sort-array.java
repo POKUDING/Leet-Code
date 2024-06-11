962d4a562cfd8f292e82239b1f86a9f1349b29c4
JunHyung Park <yoseb95@gmail.com>
2024-06-11 10:20:39 +0900

    Time: 1 ms (73.84%), Space: 41.7 MB (91.9%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -7,14 +7,14 @@ class Solution {
         for(int num : arr1)
             ++countNums[num];
         for(int arr2Num : arr2) {
-            while(countNums[arr2Num] > 0) {
-                --countNums[arr2Num];
+            while(countNums[arr2Num]-- > 0) {
+                // --countNums[arr2Num];
                 rtn[idx++] = arr2Num;
             }
         }
         for(int i = 0; i < 1001; ++ i) {
-            while(countNums[i] > 0) {
-                --countNums[i];
+            while(countNums[i]-- > 0) {
+                // --countNums[i];
                 rtn[idx++] = i;
             }
         }
