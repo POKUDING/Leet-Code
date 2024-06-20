94288000e4e9e210f2019de00dcc95f0a8a84083
JunHyung Park <yoseb95@gmail.com>
2024-06-20 10:40:30 +0900

    Time: 35 ms (37.32%), Space: 55.1 MB (90.44%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,20 +1,25 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Arrays.sort(nums);
-        int l = 0;
-        int r = nums.length - 1;
-        int ans = 0;
+        //사전검증로직
+        if(nums.length < 2 || k < 1) return 0;
+        int count = 0;
 
-        while(l < r) {
-            if(nums[l] + nums[r] == k) {
-                ++ans;
-                ++l;
-                --r;
-            } else if (nums[l] + nums[r] > k)
-                --r;
-            else
-                ++l;
+        Map<Integer, Integer> map = new HashMap<>(); //배열에 있는 각 숫자의 빈도를 저장할 해시맵 생성
+        for(int i = 0; i < nums.length; i++) {
+            int temp = k - nums[i];
+
+            if(map.containsKey(temp)) { //containsKey(temp) 메서드는 맵에 temp라는 키가 포함되어 있는지 확인하는 메서드이다.
+                count++;
+
+                if(map.get(temp) == 1) { //temp 키의 값이 1이면 //개수를 말하는 건가?
+                    map.remove(temp); //temp 키를 맵에서 제거
+                } else { //temp 키의 값이 1이 아니면
+                    map.put(temp, map.get(temp) - 1); //temp 키의 값을 1 감소
+                }
+            }else { //temp 키가 맵에 존재하지 않으면
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1); //nums[i] 키의 값을 1 증가시키거나, 없으면 1로 초기화
+            }
         }
-        return ans;
+       return count;
     }
 }
