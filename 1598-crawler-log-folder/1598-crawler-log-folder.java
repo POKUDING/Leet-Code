f219fee21611dae3f9b55dc477029a853233f7ce
JunHyung Park <yoseb95@gmail.com>
2024-07-10 09:23:00 +0900

    Time: 1 ms (99.42%), Space: 42.1 MB (26.85%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.java
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -2,10 +2,7 @@ class Solution {
     public int minOperations(String[] logs) {
         int curr = 0;
         for(String log : logs) {
-            if(log.equals("../"))
-                curr -= curr > 0 ? 1 : 0;
-            else if(!log.equals("./"))
-                ++curr;
+            curr += log.charAt(1) == '.' ? curr > 0 ? -1 : 0 : log.charAt(0) == '.' ? 0 : 1;
         }
         return curr;
     }
