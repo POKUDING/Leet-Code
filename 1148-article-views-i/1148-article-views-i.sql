79a4404d480bc2ef579bd4d95020fae29cecc192
JunHyung Park <yoseb95@gmail.com>
2024-12-27 10:52:43 +0900

    Time: 592 ms (28.09%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select author_id "id" from Views where author_id=viewer_id group by article_id order by id
+select distinct author_id as id from Views where author_id = viewer_id order by id
