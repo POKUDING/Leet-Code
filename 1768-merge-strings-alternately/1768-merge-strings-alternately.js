223585b7edfe920aa6d3f3c78a85f852d2e5d9de
JunHyung Park <yoseb95@gmail.com>
2025-04-21 11:37:33 +0900

    Time: 38 ms (82.45%), Space: 54.5 MB (26.04%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -4,20 +4,15 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let rtn = new String();
+    let rtn = "";
     let i = 0;
 
-    while(i < word1.length && i < word2.length) {
-        rtn += word1.charAt(i);
-        rtn += word2.charAt(i);
+    while(i < word1.length || i < word2.length) {
+        if(i < word1.length)
+            rtn += word1.charAt(i);
+        if(i < word2.length)
+            rtn += word2.charAt(i);
         ++i;
     }
-
-    while(i < word1.length)
-        rtn += word1.charAt(i++);
-
-    while(i < word2.length)
-        rtn += word2.charAt(i++);
-
     return rtn;
 };
