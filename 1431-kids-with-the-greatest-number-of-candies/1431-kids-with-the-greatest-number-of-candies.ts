ab89bac37e56692fa2bade0ad47a142fd79d6237
JunHyung Park <yoseb95@gmail.com>
2025-04-21 14:02:53 +0900

    Time: 1 ms (31.45%), Space: 56.3 MB (82.52%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
@@ -1,17 +1,9 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-    let max: number = 0;
+    const max: number = Math.max(...candies);
+    const rtn : boolean[] = [];
 
-    for(let num of candies)
-        max = Math.max(max, num);
-
-    const rtn : boolean[] = new Array(candies.length);
-
-    for(let i = 0; i < candies.length; ++i) {
-        if(candies[i] + extraCandies >= max)
-            rtn[i] = true;
-        else
-            rtn[i] = false;
-    }
+    for(const candy of candies)
+        rtn.push(candy + extraCandies >= max);
 
     return rtn;
 };
