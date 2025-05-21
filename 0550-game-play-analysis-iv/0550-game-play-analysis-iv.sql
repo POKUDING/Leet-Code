755cf1437229e9cf37fc4810503ce9061ad7748a
JunHyung Park <yoseb95@gmail.com>
2025-05-21 17:39:08 +0900

    Time: 596 ms (46.23%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,18 @@
+select
+    round(
+        count(a1.player_id) /
+        (select count(distinct a3.player_id) from Activity a3)
+    , 2) as fraction
+from
+    Activity a1
+where
+    (
+        (a1.player_id, date_sub(a1.event_date, interval 1 day)) in (
+            select
+                a2.player_id, min(a2.event_date)
+            from
+                Activity a2
+            group by
+                a2.player_id
+        )
+    );
